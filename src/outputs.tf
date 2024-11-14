8b1a44435a8050496809f5005766c93259109d5b
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:23:21 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,14 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "job_id" {
+  description = "Glue job ID"
+  value       = module.glue_job.id
+}
+
+output "job_name" {
+  description = "Glue job name"
+  value       = module.glue_job.name
+}
+
+output "job_arn" {
+  description = "Glue job ARN"
+  value       = module.glue_job.arn
 }
